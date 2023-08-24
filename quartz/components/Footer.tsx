a787e9367fa2e5286a0c876650255d0b1bae1b7a
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-08-24 19:00:15 +0800

    fix: break point for name

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
       <footer>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © 2020–{year} Je Sian Keith Herman
+          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © 2020–{year} Je Sian Keith Herman
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
